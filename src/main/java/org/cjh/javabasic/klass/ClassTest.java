3ea5191df91bfad47bfd1c555df9e4371043169c
maskedmarker <changjianghui0822@gmail.com>
2018-06-30 18:02:16 +0800

    m

--- src/main/java/org/cjh/javabasic/klass/ClassTest.java
+++ src/main/java/org/cjh/javabasic/klass/ClassTest.java
@@ -27,6 +27,15 @@ public class ClassTest {
 	@Test
 	public void arrayType() {
 		System.out.println(Object[].class);
+		System.out.println(boolean[].class);
+		System.out.println(byte[].class);
+		System.out.println(char[].class);
+		System.out.println(short[].class);
+		System.out.println(int[].class);
+		System.out.println(long[].class);
+		System.out.println(float[].class);
+		System.out.println(double[].class);
+//		System.out.println(void[].class); //void[] is an invalid type
 	}
 	
 
